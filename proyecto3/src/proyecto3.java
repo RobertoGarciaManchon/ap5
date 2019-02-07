19d685685ea4ad56ac6ce82ae3013032ae796bbb
RobertoGarciaManchon <robertogm3@gmail.com>
2019-02-07 13:31:27 +0100

    añadido saludo

--- proyecto3/src/proyecto3.java
+++ proyecto3/src/proyecto3.java
@@ -3,7 +3,7 @@ public class proyecto3 {
 
 	public static void main(String[] args) {
 		// TODO Apéndice de método generado automáticamente
-//inico del proyecto. vacío
+		System.out.println("hola");
 	}
 
 }
