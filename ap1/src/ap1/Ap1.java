630373cbd682bcfad0f22188cbfa18bf6a23b4b5
RobertoGarciaManchon <robertogm3@gmail.com>
2019-02-10 20:19:22 +0100

    proyecto inicial

---
+++ ap1/src/ap1/Ap1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ap1;
+
+/**
+ *
+ * @author Roberto
+ */
+public class Ap1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //primera versión del proyecto
+    }
+    
+}
